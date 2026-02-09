88c2ff949bfcf7316025b113790a4b0021bbd7f6
windme2 <windme2@gmail.com>
2026-02-09 10:00:00 +0700

    feat: add API routes for auth (NextAuth) and UploadThing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,19 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  tripImage: f({
+    image: { maxFileSize: "4MB", maxFileCount: 5 },
+  }).onUploadComplete(async ({ file }) => {
+    return { url: file.ufsUrl };
+  }),
+
+  receiptImage: f({
+    image: { maxFileSize: "4MB", maxFileCount: 1 },
+  }).onUploadComplete(async ({ file }) => {
+    return { url: file.ufsUrl };
+  }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
